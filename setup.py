7f98b07356a241c8dee877176252f73c9b926117
Kyle Scheuing <kyle@kylescheuing.com>
2021-06-04 21:40:40 -0400

    Version 1.0.1

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ import setuptools
 
 setup(
     name="huokanadvertisertoolspackager",
-    version="1.0.0",
+    version="1.0.1",
     description="Configures and packages the HuokanAdvertiserTools addon",
     author="Oppzippy",
     author_email="oppzippy@gmail.com",
